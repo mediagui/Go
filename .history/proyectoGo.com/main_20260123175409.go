2b30b08439a77635645c0cf088008e2316a02170
Miguel Angel Medina <mediagui@gmail.com>
2026-01-23 17:54:53 +0100

    refactor: restructure project by moving files to ProyectoGo directory and reintroducing ejercicios and variables packages

---
+++ .history/proyectoGo.com/main_20260123175409.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"ejercicios"
+	"fmt"
+)
+
+// Para importar varios paquetes, sepárelos con paréntesis.
+// import (
+//     "fmt"
+//     "os"
+// )
+
+// La función main es el punto de entrada de un programa Go.
+func main() {
+	fmt.Println("Hello, World! 🌍")
+	fmt.Println("Miguel Angel\nMedina")
+	ejercicios.MostrarNombre()
+
+}
