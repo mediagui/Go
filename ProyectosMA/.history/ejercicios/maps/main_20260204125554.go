97d4f08785b9604821c910e0c07bee93e6cdaba3
Miguel <mediagui@gmail.com>
2026-02-04 12:58:40 +0100

    map example history update

---
+++ ProyectosMA/.history/ejercicios/maps/main_20260204125554.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	printWeekdays(getWeekDaysMap())
+
+	weekDayMap := getWeekDaysMap()
+	requestedDay := requestWeekDayFromConsole()
+
+	printRequestedDayFrom(weekDayMap, requestedDay)
+
+}
+
+func requestWeekDayFromConsole() int {
+
+	var dayOrder int
+
+	fmt.Print("Insert day order: ")
+	fmt.Scanln(&dayOrder)
+	return dayOrder
+}
+
+func getWeekDaysMap() map[int]string {
+	weekDay := map[int]string{}
+
+	for i := range 7 {
+
+		weekDay[i] = time.Weekday(i).String()
+
+	}
+
+	return weekDay
+}
+
+func printWeekdays(w map[int]string) {
+
+	for v, k := range w {
+
+		println(v, k)
+
+	}
+
+}
+
+func printRequestedDayFrom(w map[int]string, dayOrder int) {
+	println(w[dayOrder])
+}
