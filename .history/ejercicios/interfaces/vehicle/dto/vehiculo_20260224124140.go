4153ea1e16805dc59fdc5ab18f09ed53ef00c765
Miguel <mediagui@gmail.com>
2026-02-24 13:04:20 +0100

    feat(main): implement vehicle management program with driver actions and vehicle details

---
+++ .history/ejercicios/interfaces/vehicle/dto/vehiculo_20260224124140.go
@@ -0,0 +1,97 @@
+// Package dto provides data transfer objects for vehicle management.
+package dto
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gen2brain/beeep"
+)
+
+// Vehicle type constants define the different types of vehicles supported.
+const (
+	// Car represents a car/automobile vehicle type.
+	Car VehicleType = iota + 1
+	// Lorry represents a truck vehicle type.
+	Lorry
+	// Motorcycle represents a motorcycle vehicle type.
+	Motorcycle
+	// Skateboard represents a scooter/skate vehicle type.
+	Skateboard
+)
+
+type Driver interface {
+	StartEngine()
+	StopEngine()
+	MakeSound()
+}
+
+// Vehicle represents a vehicle with its type, brand, model, and year.
+type Vehicle struct {
+	// Type is the type of vehicle (car, truck, motorcycle, etc.)
+	Type VehicleType
+	// Brand is the brand/make of the vehicle
+	Brand string
+	// Model is the model name of the vehicle
+	Model string
+	// Year is the year of the vehicle model
+	Year uint16
+	// Vehicle identification
+	Plate string
+	// Vehicle status
+	EngineStarted bool
+}
+
+// Starts the vehicle engine
+func (v *Vehicle) StartEngine() {
+	log.Println("Starting engine")
+	v.EngineStarted = true
+	log.Println("Engine started for vehicle", v.Plate)
+
+	log.Println("Brum, Brum!!")
+
+}
+
+// Stops the vehicle engine
+func (v *Vehicle) StopEngine() {
+	log.Println("Stopping engine")
+	v.EngineStarted = false
+	log.Println("Engine stopped for vehicle", v.Plate)
+}
+
+func (v Vehicle) MakeSound() {
+	switch v.Type {
+	case Lorry:
+		// Truck: lower frequency and longer duration
+		beeep.Beep(600, 600)
+	case Car:
+		beeep.Beep(800, 600)
+	case Motorcycle:
+		beeep.Beep(1000, 600)
+	default:
+		// Standard sound for other vehicles
+		beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+	}
+
+	log.Println("A ", v.Type.String(), "is beeping")
+}
+
+// String returns the string representation of the Vehicle.
+// It implements the fmt.Stringer interface by delegating to VehicleType.String().
+func (v Vehicle) String() string {
+	return fmt.Sprintf("[%s]: %s %s %s %d", v.Type.String(), v.Brand, v.Model, v.Plate, v.Year)
+}
+
+// NewVehicle creates and returns a new Vehicle pointer from the provided Vehicle value.
+// This constructor creates a copy of the vehicle data on the heap.
+func NewVehicle(auto Vehicle) *Vehicle {
+	log.Println("Building a new", auto.Type.String())
+
+	return &Vehicle{
+		Type:  auto.Type,
+		Brand: auto.Brand,
+		Model: auto.Model,
+		Year:  auto.Year,
+		Plate: auto.Plate,
+	}
+}
---
+++ .history/ejercicios/interfaces/vehicle/main_20260224124204.go
@@ -0,0 +1,134 @@
+// Tema 3 Ejercicio 7: Creación de autos
+// Crear un programa en go que permita gestionar un concesionario, creando automóviles.
+// De cada auto, se deberán especificar:
+// v Tipo (coche, camión, moto, etc…)
+// v Marca.
+// v Modelo.
+// v Matrícula.
+// v Año del modelo.
+
+// Se deberán especificar las acciones para cada coche:
+// v  Arrancar.
+// v  Frenar.
+// v  Claxon.
+package main
+
+import (
+	"log"
+	"vehicle/dto"
+)
+
+func main() {
+
+	vehiculos := buildDrivers()
+
+	printVehicleDetails(vehiculos)
+
+	startEngines(vehiculos)
+
+	makeClaxonSound(vehiculos)
+
+	stopEngines(vehiculos)
+
+}
+
+func startEngines(vehiculos map[string]dto.Driver) {
+	for _, v := range vehiculos {
+		v.StartEngine()
+	}
+}
+
+func stopEngines(vehiculos map[string]dto.Driver) {
+	for _, v := range vehiculos {
+		v.StopEngine()
+	}
+}
+
+func makeClaxonSound(vehiculos map[string]dto.Driver) {
+	for _, v := range vehiculos {
+		v.MakeSound()
+	}
+}
+
+func printVehicleDetails(vehiculos map[string]dto.Driver) {
+	for _, v := range vehiculos {
+
+		log.Printf("Vehicle: %v", v)
+
+	}
+}
+
+func buildDrivers() map[string]dto.Driver {
+
+	vehiculos := make(map[string]dto.Driver)
+
+	coche := dto.NewVehicle(dto.Vehicle{
+		Type:  dto.Car,
+		Brand: "Toyota",
+		Model: "Corolla",
+		Year:  uint16(2020),
+		Plate: "4866-BRJ",
+	})
+
+	camion := dto.NewVehicle(dto.Vehicle{
+		Type:  dto.Lorry,
+		Brand: "Pegaso",
+		Model: "Troner",
+		Year:  uint16(1985),
+		Plate: "1234-ABC",
+	})
+
+	moto := dto.NewVehicle(dto.Vehicle{
+		Type:  dto.Motorcycle,
+		Brand: "Honda",
+		Model: "CBR",
+		Year:  uint16(1990),
+		Plate: "5678-DEF",
+	})
+
+	vehiculos[coche.Type.String()] = coche
+	log.Println("Adding a", coche.Type.String())
+	vehiculos[camion.Type.String()] = camion
+	log.Println("Adding a", camion.Type.String())
+	vehiculos[moto.Type.String()] = moto
+	log.Println("Adding a", moto.Type.String())
+
+	return vehiculos
+}
+
+func buildVehicles() map[string]dto.Vehicle {
+	vehiculos := make(map[string]dto.Vehicle)
+
+	coche := dto.NewVehicle(dto.Vehicle{
+		Type:  dto.Car,
+		Brand: "Toyota",
+		Model: "Corolla",
+		Year:  uint16(2020),
+		Plate: "4866-BRJ",
+	})
+
+	camion := dto.NewVehicle(dto.Vehicle{
+		Type:  dto.Lorry,
+		Brand: "Pegaso",
+		Model: "Troner",
+		Year:  uint16(1985),
+		Plate: "1234-ABC",
+	})
+
+	moto := dto.NewVehicle(dto.Vehicle{
+		Type:  dto.Motorcycle,
+		Brand: "Honda",
+		Model: "CBR",
+		Year:  uint16(1990),
+		Plate: "5678-DEF",
+	})
+
+	vehiculos[coche.Type.String()] = *coche
+	log.Println("Adding a", coche.Type.String())
+	vehiculos[camion.Type.String()] = *camion
+	log.Println("Adding a", camion.Type.String())
+	vehiculos[moto.Type.String()] = *moto
+	log.Println("Adding a", moto.Type.String())
+
+	return vehiculos
+}
