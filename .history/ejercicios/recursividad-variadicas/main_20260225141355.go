7812cfcd5d1e7e65335805d96588570853214c5c
Miguel <mediagui@gmail.com>
2026-02-25 14:14:24 +0100

    feat: implement initial structure for recursive, anonymous, and variadic functions in Go

---
+++ .history/ejercicios/recursividad-variadicas/main_20260225141355.go
@@ -0,0 +1,11 @@
+// Se solicita crear un programa en Go, que realice lo siguiente:
+// 1.    Función recursiva:
+// Crear una función recursiva llamada [factorial] que reciba un número entero y devuelva su factorial.
+// 2.    Función anónima:
+// Dentro de [main], crear una función anónima que reciba un número entero y devuelva su cuadreado. Llamarla al menos dos veces
+// 3.    Función varíadica:
+// Crear una función [sumarTodo] que acepte un número variable de enteros y devuelva la suma de todos ellos. Probar con números distintos.
+
+package main
+
+func main() {}
